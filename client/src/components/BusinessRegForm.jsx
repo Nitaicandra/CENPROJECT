51cc718e532a2ca009134d6280569463ec4dd0c8
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2024-07-15 18:30:25 -0300

    added an empty option to state to avoid issues with backend

--- client/src/components/BusinessRegForm.jsx
+++ client/src/components/BusinessRegForm.jsx
@@ -187,6 +187,7 @@ const BusinessRegForm = ({
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                   >
+                    <option> </option>
                     <option>Alabama</option>
                     <option>Alaska</option>
                     <option>Arizona</option>
