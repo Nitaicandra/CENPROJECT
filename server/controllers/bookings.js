c386ed1936ff475f2f8318d6a10e024b71084185
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2024-07-23 20:55:33 -0300

    added GET for upcoming bookings

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -16,10 +16,10 @@ const getTokenFrom = request => {
 
 const isWithinBusinessHours = (weekday, startTime, endTime, businessHours) => {
     let valid = false;
-    for (obj of businessHours){
-        if(weekday in obj){
+    for (obj of businessHours) {
+        if (weekday in obj) {
             let availability = obj[weekday][0];
-            if (startTime >= availability.start && endTime <= availability.end){
+            if (startTime >= availability.start && endTime <= availability.end) {
                 valid = true
                 break;
             }
@@ -31,22 +31,22 @@ const isWithinBusinessHours = (weekday, startTime, endTime, businessHours) => {
 
 const isAvailable = (date, startTime, endTime, bookings) => {
     let available = true;
-    if (bookings.length === 0){ return available; }
+    if (bookings.length === 0) { return available; }
 
     const bookingsOnDate = bookings.filter(booking => booking.date === date);
-    if (bookingsOnDate.length === 0){ return available; }
+    if (bookingsOnDate.length === 0) { return available; }
 
-    for (const booking of bookingsOnDate){
-        if (endTime >= booking.startTime && startTime <= booking.endTime){
+    for (const booking of bookingsOnDate) {
+        if (endTime >= booking.startTime && startTime <= booking.endTime) {
             available = false;
             break;
         }
     }
-    
+
     return available;
 }
 
-bookingsRouter.post('/book-service/:serviceId', async(request, response) => {
+bookingsRouter.post('/book-service/:serviceId', async (request, response) => {
     // Creates a booking with a provider for a service
     const token = getTokenFrom(request)
     if (!token) {
@@ -59,38 +59,38 @@ bookingsRouter.post('/book-service/:serviceId', async(request, response) => {
     }
 
     const user = await Auth.findById(decodedToken.id)
-    if (user.accType !== 'customer'){
+    if (user.accType !== 'customer') {
         return response.status(403).json({ error: 'not a customer account' });
     }
 
-    const customer = await Customer.findOne({"login": {_id: decodedToken.id}}).populate('bookings');
-    if (!customer){
+    const customer = await Customer.findOne({ "login": { _id: decodedToken.id } }).populate('bookings');
+    if (!customer) {
         return response.status(404).json({ error: 'no customer account attached to this user' });
     }
 
     const serviceId = request.params.serviceId;
     const service = await Service.findById(serviceId).populate('provider')
-    if (!service){
+    if (!service) {
         return response.status(404).json({ error: `no service found with id ${serviceId}` });
     }
 
     const business = await Business.findById(service.provider._id).populate('bookings');
-    if (!business){
+    if (!business) {
         return response.status(404).json({ error: `no business found with id ${service.provider._id}` });
     }
 
     const { date, weekDay, startTime, endTime } = request.body;
 
     const businessHours = JSON.parse(business.availability);
-    if (!isWithinBusinessHours(weekDay, startTime, endTime, businessHours)){
+    if (!isWithinBusinessHours(weekDay, startTime, endTime, businessHours)) {
         return response.status(400).json({ error: `cannot book outside of business hours` });
     }
 
-    if (!isAvailable(date, startTime, endTime, customer.bookings)){
+    if (!isAvailable(date, startTime, endTime, customer.bookings)) {
         return response.status(400).json({ error: `customer already has a conflicting booking` });
     }
 
-    if (!isAvailable(date, startTime, endTime, business.bookings)){
+    if (!isAvailable(date, startTime, endTime, business.bookings)) {
         return response.status(400).json({ error: `business is unavailable at requested times` });
     }
 
@@ -112,6 +112,41 @@ bookingsRouter.post('/book-service/:serviceId', async(request, response) => {
 
     response.status(201).json(savedBooking);
 })
-bookingsRouter.get('/', async(request, response) => {})
+
+bookingsRouter.get('/future', async (request, response) => {
+    // Get all upcoming bookings of the logged-in user
+    const token = getTokenFrom(request)
+    if (!token) {
+        return response.status(401).json({ error: 'user is not logged in' });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!decodedToken || !decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' });
+    }
+
+    const user = await Auth.findById(decodedToken.id);
+    let account
+
+    if (user.accType !== 'business' && user.accType !== 'customer') {
+        return response.status(403).json({ error: 'not a customer nor business account' });
+    }
+
+    if (user.accType === 'business') {
+        account = await Business.findOne({ "login": { _id: decodedToken.id } }).populate('bookings');
+    } else {
+        account = await Customer.findOne({ "login": { _id: decodedToken.id } }).populate('bookings');
+    }
+
+    let today = new Date();
+    today = today.toISOString().split('T')[0];
+
+    let upcomingBookings = [];
+    for (const booking of account.bookings) {
+        if (booking.date >= today) { upcomingBookings.push(booking) }
+    }
+
+    response.status(200).json(upcomingBookings);
+})
 
 module.exports = bookingsRouter;
